91c502c411ec80c7ee3ccd1deb25fe6255a75c31
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2026-01-03 17:39:45 +0000

    revert react part.

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function greet(person) {
     return `Hello ${person}, GM.`;
 }
 const username = "Buttan";
 console.log(greet(username));
+export {};
 //# sourceMappingURL=index.js.map
